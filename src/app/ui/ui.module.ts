24f4a11ef7f5560d07ede2669c03c8b6e533c73f
develop-build <anubhavj1357@gmail.com>
2018-07-02 19:09:50 +0530

    Import GoTopButtonModule and BrowserAnimationModule to support the Got to top button

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GoTopButtonModule } from "ng2-go-top-button";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from '../app-routing.module';
 
@@ -10,7 +13,9 @@ import { LayoutComponent } from './layout/layout.component';
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoTopButtonModule,
+    BrowserAnimationsModule
   ],
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   exports: [LayoutComponent]
